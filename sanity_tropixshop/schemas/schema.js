caad035ee6073096dac708165a0f1dfeb45c33ce
Guilherme Regis <guilhermeb.regis@gmail.com>
2022-04-25 10:52:49 -0300

    sanity schema config

--- sanity_tropixshop/schemas/schema.js
+++ sanity_tropixshop/schemas/schema.js
@@ -1,10 +1,10 @@
 import createSchema from 'part:@sanity/base/schema-creator'
-
 import schemaTypes from 'all:part:@sanity/base/schema-type'
+
+import product from './product';
+import banner from './banner';
 
 export default createSchema({
   name: 'default',
-  types: schemaTypes.concat([
-  
-  ]),
+  types: schemaTypes.concat([ product, banner ]),
 })
