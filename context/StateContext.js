7fcd84dc03d0df2bb367b25c13cf77991cb7f272
Guilherme Regis <guilhermeb.regis@gmail.com>
2022-05-04 12:14:16 -0300

    sets increment logic to cart

--- context/StateContext.js
+++ context/StateContext.js
@@ -10,6 +10,9 @@ export const StateContext = ({ children }) => {
     const [totalQuantities, setTotalQuantities] = useState(0);
     const [qty, setQty] = useState(1);
 
+    let foundProduct;
+    let index;
+
     const onAdd = (product, quantity) => {
         const checkProductInCart = cartItems.find((item) => item._id === product._id);
 
@@ -33,6 +36,21 @@ export const StateContext = ({ children }) => {
             setCartItems([...cartItems, {...product}])
         };
         toast.success(`${qty} ${product.name} added to the cart.`);
+    };
+
+    const toggleCartItemQuantity = (id, value) => {
+        foundProduct = cartItems.find((item) => item._id === id);
+        index = cartItems.find((index) => product._id === id);
+
+        if (value === 'inc') {
+            let newCartQuantity = [...cartItems, { ...foundProduct, quantity: foundProduct.quantity += 1}]
+            cartItems[index] = foundProduct;
+            setCartItems(newCartQuantity);
+            setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price);
+            setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + 1);
+        } else if (value === 'dec') {
+
+        };
     };
 
     const incQty = () => {
