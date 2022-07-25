f7feb227f933b4aab2c27e1552e75fdf5d2daa88
Guilherme Regis <guilhermeb.regis@gmail.com>
2022-07-24 21:43:01 -0300

    fixes banner button

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -12,7 +12,7 @@ const HeroBanner = ({ heroBanner }) => {
         <img src={urlFor(heroBanner.image)} alt="hero-image" className="hero-banner-image"/>
       
         <div>
-          <Link href={`/product/${heroBanner.product}`}>
+          <Link href={`product/fender-stratocaster-jimi-hendrix-signature`}>
             <button type="button">{heroBanner.buttonText}</button>
           </Link>
           <div className="desc">
